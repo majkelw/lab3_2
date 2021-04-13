8074ec826a02d02863ecc38500cfb02493e23fdb
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-04-13 23:49:04 +0200

    modified class Order, added clock

--- src/main/java/edu/iis/mto/time/Order.java
+++ src/main/java/edu/iis/mto/time/Order.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.time;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -11,11 +12,17 @@ public class Order {
     private State orderState;
     private List<OrderItem> items = new ArrayList<>();
     private LocalDateTime subbmitionDate;
+    private Clock clock;
 
     public Order() {
         orderState = State.CREATED;
+        clock = Clock.systemDefaultZone();
     }
 
+    public Order(Clock clock){
+        this.clock = clock;
+        orderState = State.CREATED;
+    }
     public void addItem(OrderItem item) {
         requireState(State.CREATED, State.SUBMITTED);
 
@@ -28,13 +35,13 @@ public class Order {
         requireState(State.CREATED);
 
         orderState = State.SUBMITTED;
-        subbmitionDate = LocalDateTime.now();
+        subbmitionDate = LocalDateTime.now(clock);
 
     }
 
     public void confirm() {
         requireState(State.SUBMITTED);
-        long hoursElapsedAfterSubmittion = subbmitionDate.until(LocalDateTime.now(), ChronoUnit.HOURS);
+        long hoursElapsedAfterSubmittion = subbmitionDate.until(LocalDateTime.now(clock), ChronoUnit.HOURS);
         if (hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS) {
             orderState = State.CANCELLED;
             throw new OrderExpiredException();
