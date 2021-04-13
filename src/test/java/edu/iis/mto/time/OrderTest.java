299a24c531ebdd9bf14ec471c30ff4cba822abb3
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-04-13 23:50:12 +0200

    test invalid period, method order confirm should throw exception

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,43 @@
 package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
+
+    @Mock
+    private Clock clock;
+    private Order order;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp(){
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        order = new Order(clock);
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testOrderConfirmWithInvalidPeriodShouldThrowOrderExpiredException(){
+        int timeAfterTwoDays = 48;
+        Instant start = Instant.now();
+        Instant end = start.plus(timeAfterTwoDays, ChronoUnit.HOURS);
+        when(clock.instant()).thenReturn(start).thenReturn(end);
+        order.submit();
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
+        assertEquals(order.getOrderState(), Order.State.CANCELLED);
     }
+
 
 }
